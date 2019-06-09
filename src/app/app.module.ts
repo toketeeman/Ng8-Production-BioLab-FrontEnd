030f14ff6f92b41b9e4fb6dcded1ddce747f31cc
Allison Davis <adavis@Allisons-MacBook-Air.local>
2019-06-09 14:25:42 -0700

    Add basic styles; Angular-material menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { MatMenuModule } from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
