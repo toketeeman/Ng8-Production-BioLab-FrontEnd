1085f126a7d13cefacaa53e13efc2e7a46ff81e9
Allison Davis <adavis@Allisons-MacBook-Air.local>
2019-06-09 17:33:03 -0700

    install prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { MatMenuModule } from '@angular/material';
 import { AppComponent } from './app.component';
-import { NewTargetComponent } from "./new-target/new-target.component";
+import { NewTargetComponent } from './new-target/new-target.component';
 import { NewSubunitComponent } from './new-subunit/new-subunit.component';
 
 @NgModule({
