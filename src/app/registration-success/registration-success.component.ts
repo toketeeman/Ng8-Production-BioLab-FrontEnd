5e422449ca0e5e44048e5247a8c72c1d2b9ea0a1
Allison Davis <a.rae.davis@gmail.com>
2019-09-03 09:15:18 -0700

    Tie origin and destination subunits to interaction and ptm data; replace subunit ids with subunit names in registration-success view (#31)

--- src/app/registration-success/registration-success.component.ts
+++ src/app/registration-success/registration-success.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Store } from "@ngrx/store";
-import { Observable } from "rxjs";
+import { Observable, zip, Subscription } from "rxjs";
 import {
   AppState,
   selectTargetState,
@@ -14,13 +14,53 @@ import {
 export class RegistrationSuccessComponent implements OnInit, OnDestroy {
   target$: Observable<any>;
   interactions$: Observable<any>;
+  registrationSubscription: Subscription;
+  interactions: any[];
+  ptms: any[];
 
   constructor(private store: Store<AppState>) {
     this.target$ = this.store.select(selectTargetState);
     this.interactions$ = this.store.select(selectInteractionsState);
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.registrationSubscription = zip(
+      this.target$,
+      this.interactions$
+    ).subscribe(([target, interactionData]) => {
+      this.interactions = interactionData.interactions.map(interaction => {
+        interaction.origin_subunit_name = this.setSubunitName(
+          interaction.origin_subunit,
+          target.subunits
+        );
+        interaction.destination_subunit_name = this.setSubunitName(
+          interaction.destination_subunit,
+          target.subunits
+        );
+        return interaction;
+      });
 
-  ngOnDestroy() {}
+      this.ptms = interactionData.ptms.map(ptm => {
+        ptm.origin_subunit_name = this.setSubunitName(
+          ptm.origin_subunit,
+          target.subunits
+        );
+        ptm.destination_subunit_name = this.setSubunitName(
+          ptm.destination_subunit,
+          target.subunits
+        );
+        return ptm;
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this.registrationSubscription.unsubscribe();
+  }
+
+  setSubunitName(interactionId: any, subunits: any[]) {
+    return subunits.filter(unit => {
+      return parseInt(unit.subunit_id, 10) === parseInt(interactionId, 10);
+    })[0].subunit_name;
+  }
 }
