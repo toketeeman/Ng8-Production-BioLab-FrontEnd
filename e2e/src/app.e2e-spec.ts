ba2276c532d2043c850c4ba54535afacfb1eb1cf
Dwight Spencer <dspencer@absci.com>
2019-11-17 10:16:43 -0800

    On-going protractor test dev

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from "./app.po";
-import { browser, logging } from "protractor";
+import { browser, element, by, ExpectedConditions, logging } from "protractor";
 
 describe("workspace-project App", () => {
   let page: AppPage;
