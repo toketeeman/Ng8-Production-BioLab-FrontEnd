5de11f08fe711b0dd273cda2091e52cc22916f14
Dwight Spencer <dspencer@absci.com>
2019-11-17 08:44:01 -0800

    Converted protractor to ts

--- e2e/protractor.conf.ts
+++ e2e/protractor.conf.ts
@@ -1,19 +1,15 @@
-// @ts-check
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
 
-/**
- * @type { import("protractor").Config }
- */
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome',
+    browserName: 'chrome',
 
     'goog:chromeOptions': {
       args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
@@ -25,7 +21,7 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: () => {}
   },
   onPrepare() {
     require('ts-node').register({
