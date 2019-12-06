2524e7c55404542dfaadac0f192e7b053cf0fb34
Dwight Spencer <dspencer@absci.com>
2019-12-06 15:40:56 -0800

    removed unused code

--- src/app/services/target-detail-store.service.ts
+++ src/app/services/target-detail-store.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of, BehaviorSubject, Subscription } from 'rxjs';
+import { Observable, BehaviorSubject, Subscription } from 'rxjs';
 import { Router } from "@angular/router";
 
 import {
