658f36b2cf160a50e3e45c065664db988c3cf9d7
Dwight Spencer <dspencer@absci.com>
2019-12-31 15:22:47 -0800

    added biophysical props page

--- src/app/searches/target-property/target-property.component.ts
+++ src/app/searches/target-property/target-property.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angula
 import { HttpClient } from "@angular/common/http";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Observable, of } from "rxjs";
-import { catchError, map, take, takeUntil } from 'rxjs/operators';
+import { catchError, map, take, takeUntil, tap } from 'rxjs/operators';
+import { MatRadioChange, MatRadioButton } from '@angular/material';
 
 import { AgGridAngular } from "@ag-grid-community/angular";
 import { AllModules, Module } from "@ag-grid-enterprise/all-modules";
@@ -17,7 +18,7 @@ import { ErrorDialogService } from "../../dialogs/error-dialog/error-dialog.serv
   styleUrls: ['./target-property.component.scss']
 })
 export class TargetPropertyComponent implements OnInit, AfterViewInit {
-  @ViewChild("proteinPropertiesButton", { static: false }) proteinPropertiesButton: ElementRef<HTMLElement>;
+  @ViewChild("proteinPropertiesButton", { static: false }) proteinPropertiesButton: MatRadioButton;
   @ViewChild("targetPropertyGrid", { static: false }) targetPropertyGrid: AgGridAngular;
 
   currentTargetId: string;
@@ -25,11 +26,12 @@ export class TargetPropertyComponent implements OnInit, AfterViewInit {
   proteinName = '';
   subunitNames: string[] = [];
   propertySelectionMode: string;
-  propertyData$: Observable<ITargetProperties>;
-  propertyListData$: Observable<IGridBioProperty[]>;
+  propertyListGridData$: Observable<IGridBioProperty[]>;
   public modules: Module[] = AllModules;
   public domLayout;
   propertyColumnDefs;
+  propertyLists: ITargetPropertyList[] = [];
+
 
   constructor(
     private route: ActivatedRoute,
@@ -40,7 +42,7 @@ export class TargetPropertyComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.currentTargetId = this.route.snapshot.paramMap.get('id');
-    console.log("Target Id: ", this.currentTargetId);
+    console.log("Property Target Id: ", this.currentTargetId);
 
     if (environment.inMemoryData) {
       this.targetsPropertyUrl = environment.urls.targetsPropertyUrl;
@@ -54,12 +56,11 @@ export class TargetPropertyComponent implements OnInit, AfterViewInit {
       {
         headerName: "Property",
         headerClass: "target-property-header",
-        field: "property",
+        field: "name",
         autoHeight: true,
         cellStyle: {
           'white-space': 'normal',
           'overflow-wrap': 'break-word',
-          'font-weight': 'bold',
           width: '50%'
         },
         sortable: false,
@@ -87,7 +88,6 @@ export class TargetPropertyComponent implements OnInit, AfterViewInit {
         cellStyle: {
           'white-space': 'normal',
           'overflow-wrap': 'break-word',
-          'font-weight': 'bold',
           width: '25%'
         },
         sortable: false,
@@ -95,24 +95,27 @@ export class TargetPropertyComponent implements OnInit, AfterViewInit {
       }
     ];
 
-    this.propertyData$ = this.http.get<ITargetProperties>(this.targetsPropertyUrl)
-    .pipe(
-      catchError(error => {
-        console.log(JSON.stringify(error));
-        this.errorDialogService.openDialogForErrorResponse(
-          error,
-          ['message'],
-          "Biophysical properties for this target could not be found."
-        );
-        const noResult: ITargetProperties = null;
-        return of(noResult);
-      })
-    );
+    this.http.get<ITargetProperties>(this.targetsPropertyUrl)
+      .pipe(
+        tap((response: ITargetProperties) => {
+          this.activatePropertyListSelection(response);
+        }),
+        catchError(error => {
+          console.log(JSON.stringify(error));
+          this.errorDialogService.openDialogForErrorResponse(
+            error,
+            ['message'],
+            "Biophysical properties for this target could not be found."
+          );
+          const noResult: ITargetProperties = null;
+          return of(noResult);
+        })
+      ).subscribe();
   }
 
   ngAfterViewInit() {
     // Pre-select the protein radio button for user's convenience.
-    this.proteinPropertiesButton.nativeElement.click();
+    // this.proteinPropertiesButton.nativeElement.click();
 
     // Responsive window behavior, with debouncing.
     this.targetPropertyGrid.api.sizeColumnsToFit();
@@ -129,47 +132,72 @@ export class TargetPropertyComponent implements OnInit, AfterViewInit {
     };
   }
 
-  OnPropertySelectionChange(targetPropertyList: ITargetPropertyList) {
-    const gridPropertyList: IGridBioProperty[] = [];
-    gridPropertyList.push({ name: 'Monoisotropic MW Oxidized',
-                            value: targetPropertyList.monoiso_mw_ox,
-                            unit: 'Da'});
-    gridPropertyList.push({ name: 'Average MW Reduced',
-                            value: targetPropertyList.ave_mw_red,
-                            unit: 'Da'});
-    gridPropertyList.push({ name: 'Monoisotropic MW Reduced',
-                            value: targetPropertyList.monoiso_mw_red,
-                            unit: 'Da'});
-    gridPropertyList.push({ name: 'Isoelectric Point',
-                            value: targetPropertyList.isoelect_pt,
-                            unit: 'pH'});
-    gridPropertyList.push({ name: 'Gravy',
-                            value: targetPropertyList.gravy,
-                            unit: 'H/#Residues'});
-    gridPropertyList.push({ name: 'Aromaticity',
-                            value: targetPropertyList.aromaticity,
-                            unit: 'ppm (NICS)'});
-    gridPropertyList.push({ name: 'Molar Extinction Coefficient 280 NM Oxidized',
-                            value: targetPropertyList.mol_ext_coeff_280_nm_ox,
-                            unit: 'M^(-1) cm^(-1)'});
-    gridPropertyList.push({ name: 'Molar Extinction Coefficient 280 NM Reduced',
-                            value: targetPropertyList.mol_ext_coeff_280_nm_red,
-                            unit: 'M^(-1) cm^(-1)'});
-    gridPropertyList.push({ name: 'Molar Extinction Coefficient 214 NM',
-                            value: targetPropertyList.mol_ext_coeff_214_nm,
-                            unit: 'M^(-1) cm^(-1)'});
+  activatePropertyListSelection(response: ITargetProperties) {
+    this.propertyLists[0] = response.protein;
+    let propListButtonId = 'propList' + 0;
+    const proteinRadioButtonElement = document.getElementById(propListButtonId);
+    proteinRadioButtonElement.style.visibility = 'visible';
 
-    this.propertyListData$ = of(gridPropertyList);
+    let subunitIndex = 1;
+    for ( const subunitPropertyList of response.subunits ) {
+      this.propertyLists[subunitIndex] = subunitPropertyList;
+      propListButtonId = 'propList' + subunitIndex++;
+      document.getElementById(propListButtonId).style.visibility = 'visible';
+    }
+
+    this.proteinPropertiesButton.checked = true;
+    const change: MatRadioChange = new MatRadioChange(null, null);
+    change.value = '0';
+    this.OnPropertyListSelectionChange(change);
   }
 
-    // Go back to the current target search.
-    onBackToSearch() {
-      this.router.navigateByUrl("/home/search-targets");
-    }
+  OnPropertyListSelectionChange(change: MatRadioChange) {
+    const propertyListIndex = +change.value;
+    const propertyList = this.propertyLists[propertyListIndex];
 
-    // Go back to the current target details.
-    onBackToDetails() {
-      this.router.navigateByUrl("/home/target-detail/" + this.currentTargetId);
-    }
+    const gridPropertyList: IGridBioProperty[] = [];
+    gridPropertyList.push({ name: 'Monoisotropic MW Oxidized',
+                            value: propertyList.monoiso_mw_ox,
+                            unit: 'Da'});
+    gridPropertyList.push({ name: 'Average MW Reduced',
+                            value: propertyList.ave_mw_red,
+                            unit: 'Da'});
+    gridPropertyList.push({ name: 'Monoisotropic MW Reduced',
+                            value: propertyList.monoiso_mw_red,
+                            unit: 'Da'});
+    gridPropertyList.push({ name: 'Isoelectric Point',
+                            value: propertyList.isoelect_pt,
+                            unit: 'pH'});
+    gridPropertyList.push({ name: 'Gravy',
+                            value: propertyList.gravy,
+                            unit: 'H/#Residues'});
+    gridPropertyList.push({ name: 'Aromaticity',
+                            value: propertyList.aromaticity,
+                            unit: 'ppm (NICS)'});
+    gridPropertyList.push({ name: 'Molar Extinction Coefficient 280 NM Oxidized',
+                            value: propertyList.mol_ext_coeff_280_nm_ox,
+                            unit: 'M^(-1) cm^(-1)'});
+    gridPropertyList.push({ name: 'Molar Extinction Coefficient 280 NM Reduced',
+                            value: propertyList.mol_ext_coeff_280_nm_red,
+                            unit: 'M^(-1) cm^(-1)'});
+    gridPropertyList.push({ name: 'Molar Extinction Coefficient 214 NM',
+                            value: propertyList.mol_ext_coeff_214_nm,
+                            unit: 'M^(-1) cm^(-1)'});
+    gridPropertyList.push({ name: null,     // Work-around to avoid ag-Grid's flickering/obscuring the last data row.
+                            value: null,
+                            unit: null});
+
+    this.propertyListGridData$ = of(gridPropertyList);
+  }
+
+  // Go back to the current target search.
+  onBackToSearch() {
+    this.router.navigateByUrl("/home/search-targets");
+  }
+
+  // Go back to the current target details.
+  onBackToDetails() {
+    this.router.navigateByUrl("/home/target-detail/" + this.currentTargetId);
+  }
 
 }
