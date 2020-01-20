1f9e0d20829782c0ded7856c503532c4451d2d66
Dwight Spencer <dspencer@absci.com>
2020-01-20 12:19:17 -0800

    integration dev

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -2,12 +2,12 @@ import { browser, by, element } from "protractor";
 
 export class AppPage {
   navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+    return browser.get(browser.baseUrl);
   }
 
   getTitleText() {
     return element(
       by.css("app-root app-login-form div form h3")
-    ).getText() as Promise<string>;
+    ).getText();
   }
 }
