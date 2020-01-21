46b904d15d64ddfb73af30411abeef9f547d4bde
Dwight Spencer <dspencer@absci.com>
2020-01-21 11:36:40 -0800

    e2e test dev

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -4,23 +4,25 @@ import { environment } from "../../src/environments/environment";
 import { AppSettings } from "../../src/app/appsettings/appsettings";
 
 describe("workspace-project App", () => {
-  let page: LoginPage;
+  let loginPage: LoginPage;
   const EC = protractor.ExpectedConditions;
 
   beforeEach(async () => {
     // No preparation at this time.
   });
 
-  it('should successfully bring up login page.', async () => {
+  it('user should successfully bring up login page.', async () => {
     // Bring up the login page.
-    page = new LoginPage();
-    await page.navigateTo();
+    loginPage = new LoginPage();
+    await loginPage.navigateTo();
     const newUrl = await browser.getCurrentUrl();
     console.log("E2E - Initial URL: ", newUrl);
-    expect(await page.getTitleText()).toEqual("Log in to the AbSci Target Database");
+
+    // Check that the login page actually loaded.
+    expect(await loginPage.getTitleText()).toEqual("Log in to the AbSci Target Database");
   });
 
-  it('user1/password1 should log in successfully to correct initial page according to roles.', async () => {
+  it('valid user should log in successfully to correct initial page according to roles.', async () => {
     // Enter login credentials.
     const userName = element(by.id('username'));
     await userName.sendKeys('user1');
@@ -29,21 +31,29 @@ describe("workspace-project App", () => {
     const loginButton = await element(by.css('.btn-login'));
     await loginButton.click();
 
-    // Verify we entered correct first page after login as per our roles.
+    // Start verification that we entered correct first page after login as per our roles.
     const newUrl = await browser.getCurrentUrl();
     console.log("E2E - New URL: ", newUrl);
     const currentRoles: string[] = await browser.driver.executeScript('return JSON.parse(window.sessionStorage.getItem("currentRoles"))');
     console.log("E2E - Current Roles: ", JSON.stringify(currentRoles));
     if (currentRoles.includes(AppSettings.SUBMITTER_ROLE)) {
+
+      // Check that user with at least submitter role will reach add-target page from login.
       expect(newUrl).toContain('/home/add-target');
     } else if (currentRoles.includes(AppSettings.VIEWER_ROLE)) {
+
+      // Check that user with only viewer role will reach target search page from login.
       expect(newUrl).toContain('/home/search-targets');
     } else {
+
+      // Check that a non-submitter non-viewer user will be immediately returned to login after login attempt.
       expect(newUrl).toContain('/login');
 
-      // Verify error dialog has appeared.
+      // Start verification of an error dialog for a non-submitter non-viewer user.
       const errorHeader = element(by.css('mat-dialog-container header div'));
       browser.wait(EC.visibilityOf(errorHeader), 5000);
+
+      // Check that an error dialog has appeared for a non-submitter non-viewer user.
       expect(await errorHeader.getText()).toEqual('Error');
     }
   });
