4553e2ecbf9906bbac6d3ea382ff465400177911
Dwight Spencer <dspencer@absci.com>
2020-01-21 12:12:43 -0800

    e2e test dev

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { LoginPage } from "./app.po";
 import { browser, element, by, ExpectedConditions, logging, promise, protractor } from "protractor";
 import { environment } from "../../src/environments/environment";
 import { AppSettings } from "../../src/app/appsettings/appsettings";
+import { stringToKeyValue } from '@angular/flex-layout/extended/typings/style/style-transforms';
 
 describe("workspace-project App", () => {
   let loginPage: LoginPage;
@@ -11,30 +12,35 @@ describe("workspace-project App", () => {
     // No preparation at this time.
   });
 
-  it('user should successfully bring up login page.', async () => {
+  async function loginWithCredentials( username: string, password: string) {
+    const user = element(by.id('username'));
+    await user.sendKeys(username);
+    const pwd = element(by.id('password'));
+    await pwd.sendKeys(password);
+    const loginButton = await element(by.css('.btn-login'));
+    await loginButton.click();
+  }
+
+
+  it('Any user should successfully bring up login page.', async () => {
     // Bring up the login page.
     loginPage = new LoginPage();
     await loginPage.navigateTo();
     const newUrl = await browser.getCurrentUrl();
     console.log("E2E - Initial URL: ", newUrl);
 
-    // Check that the login page actually loaded.
+    // Check that the real login page actually loaded.
     expect(await loginPage.getTitleText()).toEqual("Log in to the AbSci Target Database");
   });
 
-  it('valid user should log in successfully to correct initial page according to roles.', async () => {
-    // Enter login credentials.
-    const userName = element(by.id('username'));
-    await userName.sendKeys('user1');
-    const password = element(by.id('password'));
-    await password.sendKeys('password1');
-    const loginButton = await element(by.css('.btn-login'));
-    await loginButton.click();
+  it('Valid user should log in successfully to correct initial page according to roles.', async () => {
+    // Enter login credentials of a valid user.
+    await loginWithCredentials('user1', 'password1');
 
     // Start verification that we entered correct first page after login as per our roles.
     const newUrl = await browser.getCurrentUrl();
     console.log("E2E - New URL: ", newUrl);
-    const currentRoles: string[] = await browser.driver.executeScript('return JSON.parse(window.sessionStorage.getItem("currentRoles"))');
+    const currentRoles: string[] | null = await browser.driver.executeScript('return JSON.parse(window.sessionStorage.getItem("currentRoles"))');
     console.log("E2E - Current Roles: ", JSON.stringify(currentRoles));
     if (currentRoles.includes(AppSettings.SUBMITTER_ROLE)) {
 
@@ -56,6 +62,18 @@ describe("workspace-project App", () => {
       // Check that an error dialog has appeared for a non-submitter non-viewer user.
       expect(await errorHeader.getText()).toEqual('Error');
     }
+  });
+
+  it('Valid user should have correct menu as per roles.', async () => {
+
+
+
+  });
+
+  it('Invalid user should be denied login.', async () => {
+
+
+
   });
 
   afterEach(async () => {
