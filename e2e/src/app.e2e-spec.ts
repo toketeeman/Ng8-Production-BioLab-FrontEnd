a03a61852107109bab96c22e9118a6f8e07f635e
Dwight Spencer <dspencer@absci.com>
2020-01-22 12:07:04 -0800

    e2e test dev

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -3,6 +3,23 @@ import { browser, element, by, ExpectedConditions, logging, promise, protractor 
 import { environment } from "../../src/environments/environment";
 import { AppSettings } from "../../src/app/appsettings/appsettings";
 
+// NOTES:
+//
+// A set of special (phantom) VALID user accounts are required to be established
+// beforehand in order to run this e2e test.
+//
+// One special valid user is needed for possessing each combination of roles, respectively.
+//
+// One additional user who is non-existant (i.e. clearly invalid) is needed. Make one up.
+//
+// This test can only be run only in the dev.local and dev.docker configurations because it will
+// need to be easily repeatable against the backend (in-memory-DB or Docker Desktop).
+//
+// If very detailed tests are added that checks the results of very specific updates or
+// very specific searches, those tests should be conditioned to run ONLY in dev.docker
+// because dev.local only simulates some of those operations. After each e2e run in dev.docker,
+// the Docker Desktop environment must be recycled to restore the initial test databases.
+
 describe("workspace-project App", () => {
   let loginPage: LoginPage;
   const EC = protractor.ExpectedConditions;
@@ -31,7 +48,7 @@ describe("workspace-project App", () => {
     loginPage = new LoginPage();
     await loginPage.navigateTo();
     const newUrl = await browser.getCurrentUrl();
-    console.log("E2E - Initial URL: ", newUrl);
+    console.log("E2E - Initial Access URL: ", newUrl);
 
     // Check that the real login page actually loaded.
     expect(await loginPage.getTitleText()).toEqual("Log in to the AbSci Target Database");
@@ -67,14 +84,50 @@ describe("workspace-project App", () => {
     }
   });
 
-  it('3. Valid user should have correct menu as per roles.', async () => {
+  it('3. Valid user should have correct menu activation as per roles.', async () => {
+    // Recycle by logging out. (Pre-condition: previous test has user logged in.)
+    await logoutToRecycle();
 
+    // Enter login credentials of an valid user.
+    await loginWithCredentials('user1', 'password1');
 
+    // Click on menu to see options.
+    let menuButton = null;
+    menuButton = await element(by.id('e2e-mat-menu'));
+    await menuButton.click();
 
+    // Inspect activation of menu options.
+    const currentRoles: string[] | null = await browser.driver.executeScript('return JSON.parse(window.sessionStorage.getItem("currentRoles"))');
+    const registerNewTargetButtonDisabled =
+      await element(by.cssContainingText('.mat-menu-content button', 'Register New Target')).getAttribute('disabled');
+    const searchTargetsButtonDisabled =
+      await element(by.cssContainingText('.mat-menu-content button', 'Search Targets')).getAttribute('disabled');
+    const searchPlasmidsButtonDisabled =
+      await element(by.cssContainingText('.mat-menu-content button', 'Search Plasmids')).getAttribute('disabled');
+
+    // Test for correct activation for submitter role.
+    if (currentRoles.includes(AppSettings.SUBMITTER_ROLE)) {
+      expect(registerNewTargetButtonDisabled).toBeFalsy();
+    } else {
+      expect(registerNewTargetButtonDisabled).toBeTruthy();
+    }
+
+    // Test for correct activation for view role.
+    if (currentRoles.includes(AppSettings.VIEWER_ROLE)) {
+      expect(searchTargetsButtonDisabled).toBeFalsy();
+      expect(searchPlasmidsButtonDisabled).toBeFalsy();
+    } else {
+      expect(searchTargetsButtonDisabled).toBeTruthy();
+      expect(searchPlasmidsButtonDisabled).toBeTruthy();
+    }
+
+    // Close the menu without making a selection. Also enables logout recycling below.
+    const body = await element(by.css('body'));
+    await browser.actions().mouseMove(body, {x: 0, y: 0}).click().perform();
   });
 
   it('4. Invalid user should be denied login.', async () => {
-    // Recycle by logging out.
+    // Recycle by logging out. (Pre-condition: previous test has user logged in.)
     await logoutToRecycle();
 
     // Enter login credentials of an invalid user.
