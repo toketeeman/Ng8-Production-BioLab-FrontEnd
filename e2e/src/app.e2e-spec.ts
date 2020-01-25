cefa2f8e9ec9f9419c19866da94f4b55c880f237
Dwight Spencer <dspencer@absci.com>
2020-01-25 14:01:27 -0800

    e2e test dev

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -59,10 +59,10 @@ describe("workspace-project App", () => {
     // intended only for aiding the development of tests in dev.local.
     //
     // Use only in-memory-DB login/password if running in dev.local.
-    if (environment.configuration === 'dev.local' && username !== 'userx') {
-      username = 'user1';
-      password = 'password1';
-    }
+    // if (environment.configuration === 'dev.local' && username !== 'userx') {
+    //   username = 'user1';
+    //   password = 'password1';
+    // }
 
     console.log("\n----E2E - Current Username: ", username);
     const user = element(by.id('username'));
@@ -214,14 +214,14 @@ describe("workspace-project App", () => {
 
   });
 
-  it('5. Valid no-role user should log in successfully to appropriate initial page according to roles.', async () => {
-    // Pre-condition: supplied user must be valid with no roles.
+  // it('5. Valid no-role user should log in successfully to appropriate initial page according to roles.', async () => {
+  //   // Pre-condition: supplied user must be valid with no roles.
 
-    await restartAppSession();
+  //   await restartAppSession();
 
-    await initialPageTest('testuser_NO_GROUPS', '7@S#HliL813C');
+  //   await initialPageTest('testuser_NO_GROUPS', '7@S#HliL813C');
 
-  });
+  // });
 
   it('6. Valid viewer-only user should have correct menu activation as per roles upon login.', async () => {
     // Pre-condition: supplied user must be valid with viewer role.
@@ -253,71 +253,71 @@ describe("workspace-project App", () => {
 
   });
 
-  it('9. Invalid user should be denied login with error message.', async () => {
-    // Pre-condition: supplied user must be invalid.
+  // it('9. Invalid user should be denied login with error message.', async () => {
+  //   // Pre-condition: supplied user must be invalid.
 
-    await restartAppSession();
+  //   await restartAppSession();
 
-    // Enter login credentials of an INVALID user.
-    await loginWithCredentials('userx', 'passwordx');
+  //   // Enter login credentials of an INVALID user.
+  //   await loginWithCredentials('userx', 'passwordx');
 
-    // Check that an invalid user will be immediately returned to login after login attempt.
-    const newUrl = await browser.getCurrentUrl();
-    expect(newUrl).toContain('/login');
+  //   // Check that an invalid user will be immediately returned to login after login attempt.
+  //   const newUrl = await browser.getCurrentUrl();
+  //   expect(newUrl).toContain('/login');
 
-    // Start verification of an error dialog for a non-submitter non-viewer user.
-    const errorHeader = element(by.css('mat-dialog-container header div'));
-    browser.wait(EC.visibilityOf(errorHeader), 5000);
+  //   // Start verification of an error dialog for a non-submitter non-viewer user.
+  //   const errorHeader = element(by.css('mat-dialog-container header div'));
+  //   browser.wait(EC.visibilityOf(errorHeader), 5000);
 
-    // Check that an error dialog has appeared for a non-submitter non-viewer user.
-    expect(await errorHeader.getText()).toEqual('Error');
+  //   // Check that an error dialog has appeared for a non-submitter non-viewer user.
+  //   expect(await errorHeader.getText()).toEqual('Error');
 
-    // Recycle for next test by dismissing error dialog.
-    const errorDialogCloseButton = await element(by.cssContainingText('footer span', 'Close'));
-    await errorDialogCloseButton.click();
-  });
+  //   // Recycle for next test by dismissing error dialog.
+  //   const errorDialogCloseButton = await element(by.cssContainingText('footer span', 'Close'));
+  //   await errorDialogCloseButton.click();
+  // });
 
-  it('10. Valid viewer user should be able to access target search page and target detail page.', async () => {
+  // it('10. Valid viewer user should be able to access target search page and target detail page.', async () => {
 
-    await restartAppSession();
+  //   await restartAppSession();
 
-    // Enter login credentials of a valid viewer user.
-    await loginWithCredentials('testuser_VIEWER', '%aEX@D4ez@DT');
-    await browser.getCurrentUrl();
+  //   // Enter login credentials of a valid viewer user.
+  //   await loginWithCredentials('testuser_VIEWER', '%aEX@D4ez@DT');
+  //   await browser.getCurrentUrl();
 
-    // Allow test only if user has at least viewer role.
-    const currentRoles: string[] =
-      await browser.driver.executeScript('return JSON.parse(window.sessionStorage.getItem("currentRoles"))');
-    console.log("\n----E2E - Current Roles: ", JSON.stringify(currentRoles));
+  //   // Allow test only if user has at least viewer role.
+  //   const currentRoles: string[] =
+  //     await browser.driver.executeScript('return JSON.parse(window.sessionStorage.getItem("currentRoles"))');
+  //   console.log("\n----E2E - Current Roles: ", JSON.stringify(currentRoles));
 
-    // Click on menu to see options. Then click on the Search Targets option.
-    let menuButton = null;
-    menuButton = await element(by.id('e2e-mat-menu'));
-    await menuButton.click();
-    const searchTargetsSelection = await element(by.cssContainingText('.mat-menu-content button', 'Search Targets'));
-    await searchTargetsSelection.click();
+  //   // Click on menu to see options. Then click on the Search Targets option.
+  //   let menuButton = null;
+  //   menuButton = await element(by.id('e2e-mat-menu'));
+  //   await menuButton.click();
+  //   const searchTargetsSelection = await element(by.cssContainingText('.mat-menu-content button', 'Search Targets'));
+  //   await searchTargetsSelection.click();
 
-    // Check that the target search page has come up.
-    const searchTargetUrl = await browser.getCurrentUrl();
-    expect(searchTargetUrl).toContain('/home/search-targets');
+  //   // Check that the target search page has come up.
+  //   const searchTargetUrl = await browser.getCurrentUrl();
+  //   expect(searchTargetUrl).toContain('/home/search-targets');
 
-    // Check that at least one target row was retrieved from the backend.
-    const allRowsOnPage = element.all(by.css('div.ag-center-cols-container div.ag-row'));
-    expect(allRowsOnPage.count()).toBeGreaterThan(0);
+  //   // Check that at least one target row was retrieved from the backend.
+  //   const allRowsOnPage = element.all(by.css('div.ag-center-cols-container div.ag-row'));
+  //   expect(allRowsOnPage.count()).toBeGreaterThan(0);
 
-    // Check that clicking some target row will retrieve the target detail page.
-    const anyRow = element(by.css('.ag-body-viewport'));
-    expect(anyRow.isPresent()).toBeTruthy();
-    await anyRow.click();
-    const targetDetailUrl = await browser.getCurrentUrl();
-    expect(targetDetailUrl).toMatch(/.+\/home\/target-detail\/\d+/);
+  //   // Check that clicking some target row will retrieve the target detail page.
+  //   const anyRow = element(by.css('.ag-body-viewport'));
+  //   expect(anyRow.isPresent()).toBeTruthy();
+  //   await anyRow.click();
+  //   const targetDetailUrl = await browser.getCurrentUrl();
+  //   expect(targetDetailUrl).toMatch(/.+\/home\/target-detail\/\d+/);
 
-    // Check that a target has been loaded into the target details page.
-
-
+  //   // Check that a target has been loaded into the target details page.
 
 
-  });
+
+
+  // });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser.
